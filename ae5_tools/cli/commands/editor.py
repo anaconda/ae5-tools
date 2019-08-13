88d96a34b6c89fbdd0eb25406a2ea2ecc6a1f34b
Michael C. Grant <mgrant@anaconda.com>
2019-08-13 13:18:26 -0500

    endpoint revamp; resource profiles and editors

---
+++ ae5_tools/cli/commands/editor.py
@@ -0,0 +1,36 @@
+import click
+
+from ..login import login_options, cluster_call
+from ..format import print_output, format_options
+
+
+@click.group(short_help='info, list',
+             epilog='Type "ae5 editor <command> --help" for help on a specific command.')
+@format_options()
+@login_options()
+def editor():
+    pass
+
+
+@editor.command()
+@format_options()
+@login_options()
+def list():
+    '''List the available editors.
+    '''
+    result = cluster_call('editor_list', format='dataframe')
+    print_output(result)
+
+
+@editor.command()
+@click.argument('name')
+@format_options()
+@login_options()
+def info(name):
+    '''Retrieve the record of a single editor.
+
+       The NAME identifier must match exactly one name of an editor.
+       Wildcards may be included.
+    '''
+    result = cluster_call('editor_info', name, format='dataframe')
+    print_output(result)
