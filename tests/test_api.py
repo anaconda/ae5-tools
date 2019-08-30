61085a756420c7e5d5ece086aeac8587dff59a21
Michael C. Grant <mgrant@anaconda.com>
2019-08-30 16:58:38 -0500

    Intercept redirect errors

--- tests/test_api.py
+++ tests/test_api.py
@@ -130,7 +130,8 @@ def test_project_activity(user_session, project_set):
     for rec0 in project_set:
         activity = user_session.project_activity('testproj3')
         assert all(rec0['owner'] == rec1['owner'] for rec1 in activity)
-        assert activity[-1]['type'] == 'create_action' and activity[-1]['done']
+        assert activity[-1]['type'] in ('create_action', 'deploy_action')
+        assert activity[-1]['done']
 
 
 def test_project_download_upload_delete(user_session, project_set, user_project_list):
