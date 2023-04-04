83483b6d15276932441d018b2917f2d70f525e75
Josh Burt <jburt@anaconda.com>
2023-04-04 10:40:50 -0700

    updated unit test coverage

--- tests/unit/ae5_tools/test_api_secret.py
+++ tests/unit/ae5_tools/test_api_secret.py
@@ -27,8 +27,8 @@ def user_session(get_token_fixture):
 
 def test_secret_add(user_session, monkeypatch):
     # Set up test
-    mock_key: str = "MOCK-KEY"
-    mock_value: str = "MOCK-VALUE"
+    mock_key: str = "MOCK_KEY"
+    mock_value: str = "MOCK_VALUE"
 
     mock_post: MagicMock = MagicMock()
 
@@ -38,18 +38,31 @@ def test_secret_add(user_session, monkeypatch):
     user_session.secret_add(key=mock_key, value=mock_value)
 
     # Review the results
-    print(mock_post.assert_called_once_with("credentials/user", json={"key": "MOCK-KEY", "value": "MOCK-VALUE"}))
+    print(mock_post.assert_called_once_with("credentials/user", json={"key": mock_key, "value": mock_value}))
 
+
+def test_secret_add_with_invalid_name(user_session, monkeypatch):
+    # Set up test
+    mock_key: str = "MOCK-KEY"
+    mock_value: str = "MOCK_VALUE"
+
+    # Execute the test
+    with pytest.raises(AEException) as context:
+        user_session.secret_add(key=mock_key, value=mock_value)
+
+    # Review the results
+    assert str(context.value) == f"User secret {mock_key} can not be created. Key contains non-alphanumeric characters."
 
 #####################################################
 # Test Cases For secret_delete
 #####################################################
 
+
 def test_secret_delete(user_session, monkeypatch):
     # Set up test
-    mock_key: str = "MOCK-KEY"
+    mock_key: str = "MOCK_KEY"
     mock_delete: MagicMock = MagicMock()
-    secret_list_mock: MagicMock = MagicMock(return_value=[{"secrets": [mock_key]}])
+    secret_list_mock: MagicMock = MagicMock(return_value=[{"secret_name": mock_key}])
     monkeypatch.setattr(AEUserSession, "secret_list", secret_list_mock)
     monkeypatch.setattr(AEUserSession, "_delete", mock_delete)
 
@@ -57,14 +70,25 @@ def test_secret_delete(user_session, monkeypatch):
     user_session.secret_delete(key=mock_key)
 
     # Review the results
-    mock_delete.assert_called_once_with("credentials/user/MOCK-KEY")
+    mock_delete.assert_called_once_with(f"credentials/user/{mock_key}")
+
+
+def test_secret_delete_with_invalid_name(user_session, monkeypatch):
+    # Set up test
+    mock_key: str = "MOCK-KEY"
+
+    # Execute the test
+    with pytest.raises(AEException) as context:
+        user_session.secret_delete(key=mock_key)
+
+    # Review the results
+    assert str(context.value) == f"User secret {mock_key} can not be deleted. Key contains non-alphanumeric characters."
 
 
 def test_secret_delete_with_missing_secret(user_session, monkeypatch):
     # Set up test
-    mock_key: str = "MOCK-KEY"
-    mock_delete: MagicMock = MagicMock()
-    secret_list_mock: MagicMock = MagicMock(return_value=[{"secrets": []}])
+    mock_key: str = "MOCK_KEY"
+    secret_list_mock: MagicMock = MagicMock(return_value=[])
     monkeypatch.setattr(AEUserSession, "secret_list", secret_list_mock)
 
     # Execute the test
@@ -72,7 +96,7 @@ def test_secret_delete_with_missing_secret(user_session, monkeypatch):
         user_session.secret_delete(key=mock_key)
 
     # Review the result
-    assert str(context.value) == "User secret 'MOCK-KEY' was not found and cannot be deleted."
+    assert str(context.value) == f"User secret {mock_key} was not found and cannot be deleted."
 
 
 #####################################################
@@ -81,7 +105,7 @@ def test_secret_delete_with_missing_secret(user_session, monkeypatch):
 
 def test_secret_list(user_session, monkeypatch):
     # Set up the test
-    mock_key: str = "MOCK-KEY"
+    mock_key: str = "MOCK_KEY"
     mock_get: MagicMock = MagicMock(return_value={"data": [mock_key]})
     monkeypatch.setattr(AEUserSession, "_get", mock_get)
 
@@ -89,12 +113,12 @@ def test_secret_list(user_session, monkeypatch):
     secrets: List[str] = user_session.secret_list()
 
     # Review the result
-    assert secrets == [{"secrets": [mock_key]}]
+    assert secrets == [{"secret_name": mock_key}]
 
 
 def test_secret_list_gracefully_fails(user_session, monkeypatch):
     # Set up the test
-    mock_key: str = "MOCK-KEY"
+    mock_key: str = "MOCK_KEY"
     mock_get: MagicMock = MagicMock(return_value={})
     monkeypatch.setattr(AEUserSession, "_get", mock_get)
 
