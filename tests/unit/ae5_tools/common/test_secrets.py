592c6741684d9740597efaebdf8d700bb1857ad6
Josh Burt <jburt@anaconda.com>
2023-04-12 15:32:25 -0700

    converted unittest to pytest

--- tests/unit/ae5_tools/common/test_secrets.py
+++ tests/unit/ae5_tools/common/test_secrets.py
@@ -1,38 +1,33 @@
 import os
-import unittest
+
 
 from ae5_tools import load_ae5_user_secrets
 
 
-class TestSecrets(unittest.TestCase):
-    def test_base(self):
-        secret_name: str = "MOCK_SECRET"
+def test_base():
+    secret_name: str = "MOCK_SECRET"
 
-        if secret_name in os.environ:
-            del os.environ[secret_name]
+    if secret_name in os.environ:
+        del os.environ[secret_name]
 
-        mock_secrets_path: str = "tests/fixtures/secrets"
+    mock_secrets_path: str = "tests/fixtures/secrets"
 
-        with open(file=f"{mock_secrets_path}/{secret_name}", mode="r", encoding="utf-8") as file:
-            expected_secret_value: str = file.read()
+    with open(file=f"{mock_secrets_path}/{secret_name}", mode="r", encoding="utf-8") as file:
+        expected_secret_value: str = file.read()
 
-        load_ae5_user_secrets(secrets_path=mock_secrets_path, silent=False)
-        load_ae5_user_secrets(secrets_path=mock_secrets_path, silent=True)
+    load_ae5_user_secrets(secrets_path=mock_secrets_path, silent=False)
+    load_ae5_user_secrets(secrets_path=mock_secrets_path, silent=True)
 
-        self.assertEqual(os.environ[secret_name], expected_secret_value)
-
-    def test_path_does_not_exist(self):
-        secret_name: str = "MOCK_SECRET"
-
-        if secret_name in os.environ:
-            del os.environ[secret_name]
-
-        load_ae5_user_secrets(secrets_path="some/path", silent=False)
-        load_ae5_user_secrets(secrets_path="some/path", silent=True)
-
-        self.assertFalse(secret_name in os.environ)
+    assert os.environ[secret_name] == expected_secret_value
 
 
-if __name__ == "__main__":
-    runner = unittest.TextTestRunner()
-    runner.run(TestSecrets())
+def test_path_does_not_exist():
+    secret_name: str = "MOCK_SECRET"
+
+    if secret_name in os.environ:
+        del os.environ[secret_name]
+
+    load_ae5_user_secrets(secrets_path="some/path", silent=False)
+    load_ae5_user_secrets(secrets_path="some/path", silent=True)
+
+    assert secret_name not in os.environ
