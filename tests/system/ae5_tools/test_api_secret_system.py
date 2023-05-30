b8b06dc885fde2c6414d071d2de79e24a290eadd
Josh Burt <jburt@anaconda.com>
2023-05-30 10:20:36 -0700

    disable other test suites during rapi ci development

--- tests/system/ae5_tools/test_api_secret_system.py
+++ tests/system/ae5_tools/test_api_secret_system.py
@@ -1,42 +1,42 @@
-import uuid
-from typing import Dict, List
-
-import pytest
-
-from ae5_tools.api import AEException, AEUserSession
-from tests.utils import _get_vars
-
-
-@pytest.fixture(scope="session")
-def user_session():
-    hostname, username, password = _get_vars("AE5_HOSTNAME", "AE5_USERNAME", "AE5_PASSWORD")
-    s = AEUserSession(hostname, username, password)
-    yield s
-    s.disconnect()
-
-
-@pytest.fixture(scope="module")
-def secret_name():
-    return "MOCK_SECRET"
-
-
-#####################################################
-# Test Cases For secret_add, secret_list, and secret_delete
-#####################################################
-
-
-def test_secret_add_and_list(user_session, secret_name):
-    user_session.secret_add(key=secret_name, value=str(uuid.uuid4()))
-    results: List[str] = user_session.secret_list()
-    assert secret_name in [secret["secret_name"] for secret in results]
-
-
-def test_secret_delete(user_session, secret_name):
-    user_session.secret_delete(key=secret_name)
-
-
-def test_secret_delete_should_gracefully_fail(user_session, secret_name):
-    new_key: str = str(uuid.uuid4()).replace("-", "_")
-    with pytest.raises(AEException) as context:
-        user_session.secret_delete(key=new_key)
-    assert str(context.value) == f"User secret {new_key} was not found and cannot be deleted."
+# import uuid
+# from typing import Dict, List
+#
+# import pytest
+#
+# from ae5_tools.api import AEException, AEUserSession
+# from tests.utils import _get_vars
+#
+#
+# @pytest.fixture(scope="session")
+# def user_session():
+#     hostname, username, password = _get_vars("AE5_HOSTNAME", "AE5_USERNAME", "AE5_PASSWORD")
+#     s = AEUserSession(hostname, username, password)
+#     yield s
+#     s.disconnect()
+#
+#
+# @pytest.fixture(scope="module")
+# def secret_name():
+#     return "MOCK_SECRET"
+#
+#
+# #####################################################
+# # Test Cases For secret_add, secret_list, and secret_delete
+# #####################################################
+#
+#
+# def test_secret_add_and_list(user_session, secret_name):
+#     user_session.secret_add(key=secret_name, value=str(uuid.uuid4()))
+#     results: List[str] = user_session.secret_list()
+#     assert secret_name in [secret["secret_name"] for secret in results]
+#
+#
+# def test_secret_delete(user_session, secret_name):
+#     user_session.secret_delete(key=secret_name)
+#
+#
+# def test_secret_delete_should_gracefully_fail(user_session, secret_name):
+#     new_key: str = str(uuid.uuid4()).replace("-", "_")
+#     with pytest.raises(AEException) as context:
+#         user_session.secret_delete(key=new_key)
+#     assert str(context.value) == f"User secret {new_key} was not found and cannot be deleted."
--- tests/test_help.py
+++ tests/test_help.py
@@ -1,36 +1,36 @@
-import os
-import shlex
-import subprocess
-
-
-def _cmd(cmd, table=True):
-    # We go through Pandas to CSV to JSON instead of directly to JSON to improve coverage
-    cmd = "ae5 " + cmd
-    print(f"Executing: {cmd}")
-    text = subprocess.check_output(shlex.split(cmd), stdin=open(os.devnull))
-    assert text.strip() != ""
-    print(text.decode())
-
-
-def test_help():
-    _cmd("--help")
-
-
-def test_help_login():
-    _cmd("--help-login")
-
-
-def test_help_format():
-    _cmd("--help-format")
-
-
-def test_project_help():
-    _cmd("project --help")
-
-
-def test_session_help():
-    _cmd("session --help")
-
-
-def test_deployment_help():
-    _cmd("deployment --help")
+# import os
+# import shlex
+# import subprocess
+#
+#
+# def _cmd(cmd, table=True):
+#     # We go through Pandas to CSV to JSON instead of directly to JSON to improve coverage
+#     cmd = "ae5 " + cmd
+#     print(f"Executing: {cmd}")
+#     text = subprocess.check_output(shlex.split(cmd), stdin=open(os.devnull))
+#     assert text.strip() != ""
+#     print(text.decode())
+#
+#
+# def test_help():
+#     _cmd("--help")
+#
+#
+# def test_help_login():
+#     _cmd("--help-login")
+#
+#
+# def test_help_format():
+#     _cmd("--help-format")
+#
+#
+# def test_project_help():
+#     _cmd("project --help")
+#
+#
+# def test_session_help():
+#     _cmd("session --help")
+#
+#
+# def test_deployment_help():
+#     _cmd("deployment --help")
