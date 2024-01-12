4521d5109e2dd5f54e3230b97c3fa7b3215da416
joshburt <jburt@anaconda.com>
2024-01-11 16:31:07 -0800

    re-enable the tests

--- tests/system/runner.py
+++ tests/system/runner.py
@@ -15,9 +15,7 @@ logger = logging.getLogger(__name__)
 
 
 def run() -> None:
-    shell_out_cmd: str = (
-        "python -m pytest --cov=ae5_tools --show-capture=all -rP tests/system/ae5_tools/test_api.py --cov-append --cov-report=xml -vv"
-    )
+    shell_out_cmd: str = "python -m pytest --cov=ae5_tools --show-capture=all -rP tests/system/ae5_tools --cov-append --cov-report=xml -vv"
 
     if get_env_var(name="CI") and demand_env_var_as_bool(name="CI"):
         shell_out_cmd += " --ci-skip"
